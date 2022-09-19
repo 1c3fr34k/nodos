99fca560ec05184a36faba84411d930648159985
1c3fr34k <icefreak0612@gmail.com>
2022-09-19 20:51:14 +0200

    test

--- nodos/settings.py
+++ nodos/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import platform
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,4 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TAILWIND_APP_NAME = 'theme'
 
 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+if platform.system() == "Windows":
+    NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
