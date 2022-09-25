386646f7923ef19979efe375b41d10950546a529
1c3fr34k <icefreak0612@gmail.com>
2022-09-25 21:42:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-def test():
-    print("terminal")
+import os
+import webview
+
+
+os.system("start cmd /k python manage.py runserver")
+webview.create_window("Nodos", "http://127.0.0.1/notes")
+webview.start()
