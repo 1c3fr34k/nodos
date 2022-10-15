992d52cecd76302243367b046f5d94d43404f943
1c3fr34k <icefreak0612@gmail.com>
2022-10-15 13:58:30 +0200

    Update models.py

--- nodos/models.py
+++ nodos/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+
 class Note(models.Model):
     title = models.TextField(max_length=150)
     description = models.CharField(max_length=2000, blank=True, null=True)
     done = models.BooleanField(default=False, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
